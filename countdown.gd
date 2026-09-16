e60bf8305837925211f546899b0da31cef5c6ab4
aeroplaneehheh <unluckyluckycloverrr@gmail.com>
2026-09-16 21:50:36 +0800

    add countdown

---
+++ countdown.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+@onready var label: Label = $Label
+@onready var timer: Timer = $Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	timer.start(50)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	label.text = "%02d:%02d" % time_left()
+	print("%02d:%02d" % time_left())
+
+
+func time_left():
+	var time_left = timer.time_left
+	var minutes = floor(time_left / 60)
+	var seconds = int(time_left) % 60
+	return [minutes, seconds]
