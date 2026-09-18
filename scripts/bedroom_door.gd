a4c3a141a48d9288a2de70d87157f3066d08a3b5
aeroplaneehheh <unluckyluckycloverrr@gmail.com>
2026-09-18 22:43:17 +0800

    add door open and close

--- scripts/bedroom_door.gd
+++ scripts/bedroom_door.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 
-@onready var interaction_area: interactionArea = $"/root/main/interactionArea"
-@onready var door_collision = $"/root/main/interactionArea/bedroomDoor"
+@onready var interaction_area: interactionArea = $"/root/main/bedroomDoor/interactionArea"
+@onready var door_collision = $"/root/main/bedroomDoor/bedroomDoorCollision"
 
 
 var state = false
@@ -20,8 +20,8 @@ func _on_interact():
 	if state == false:
 		print("opened")
 		state = true
-		door_collision.disabled = false
+		door_collision.set_deferred("disabled", true)
 	else:
 		print("closed")
 		state = false
-		door_collision.disabled = true
+		door_collision.set_deferred("disabled", false)
