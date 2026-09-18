16959d913e2e0cc57e598e4e07bd9dc508458500
aeroplaneehheh <unluckyluckycloverrr@gmail.com>
2026-09-19 00:06:21 +0800

    doors work independently from each other now

---
+++ scripts/bathroom_door.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+@onready var interaction_area: interactionArea = $"/root/main/bathroomDoor/interactionArea"
+@onready var door_collision = $"/root/main/bathroomDoor/bathroomDoorCollision"
+
+
+var state = false
+
+
+func _ready() -> void:
+	interaction_area.interact = Callable(self, "_on_interact")
+
+
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_interact():
+	if state == false:
+		print("opened")
+		state = true
+		door_collision.set_deferred("disabled", true)
+	else:
+		print("closed")
+		state = false
+		door_collision.set_deferred("disabled", false)
---
+++ scripts/master_bedroom_door.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+
+@onready var interaction_area: interactionArea = $"/root/main/masterBedroomDoor/interactionArea"
+@onready var door_collision = $"/root/main/masterBedroomDoor/masterBedroomDoorCollision"
+
+
+var state = false
+
+
+
+
+func _ready() -> void:
+	interaction_area.interact = Callable(self, "_on_interact")
+
+
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_interact():
+	if state == false:
+		print("opened")
+		state = true
+		door_collision.set_deferred("disabled", true)
+	else:
+		print("closed")
+		state = false
+		door_collision.set_deferred("disabled", false)
